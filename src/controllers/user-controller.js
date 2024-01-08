90e88d884677fdb50c1d1860add9c624b11b330d
gssp4167 <gssp4167@gmail.com>
2024-01-08 23:11:52 +0530

    Added nw user signup api

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,29 @@
+const {StatusCodes}=require('http-status-codes');
+const {UserService}=require('../services');
+const {SuccessResponse,ErrorResponse}=require('../utils/common')
+
+
+async function signup(req,res){
+    try {
+        // console.log("Inside airplane controller");
+        const user=await UserService.create({
+            email:req.body.email,
+            password:req.body.password
+        });
+        SuccessResponse.data=user;
+        return res
+        .status(StatusCodes.CREATED)
+        .json(SuccessResponse);
+    } 
+    catch (error) {
+        ErrorResponse.error=error;
+        // console.log(error);
+        return res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .json(ErrorResponse);
+    }
+}
+
+module.exports={
+    signup
+}
