1d447f27258c26120c65b74646e93e77851ddfe1
gssp4167 <gssp4167@gmail.com>
2024-01-13 23:21:57 +0530

    Added rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express=require('express');
+const rateLimit=require('express-rate-limit');
+const {createProxyMiddleware}=require('http-proxy-middleware');
 const {ServerConfig,Logger}=require('./config');
 const apiRoutes=require('./routes');
 const app=express();
 
+
+const limiter=rateLimit({
+    windowMs: 2*60*1000,
+    max:3
+});
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use(limiter);
+
+app.use('/flightsService',createProxyMiddleware({target:ServerConfig.FLIGHT_SERVICE,changeOrigin:true}));
+app.use('/bookingService',createProxyMiddleware({target:ServerConfig.BOOKING_SERVICE,changeOrigin:true}));
 
 app.use('/api',apiRoutes);
 
